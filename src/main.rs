8463696a94b0a04140841fef55e9b833bb370997
willmexe <imdark200@gmail.com>
2022-07-05 02:41:58 +0200

    Cleaned up categories.

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,7 @@ fn help_flat(message: &str) {
 const USAGE_PREFIX: &str = "usage";
 const OPTIONS_PREFIX: &str = "options";
 
-const CATEGORIES: [&str; 7] = ["anime", "manga", "movie", "book", "podcast", "liveaction", "liveactionseries"];
+const CATEGORIES: [&str; 4] = ["series", "movie", "book", "podcast"];
 
 const STATUS: [&str; 5] = ["planned", "watching", "completed", "paused", "dropped"];
 const STATUS_LOWER: [char; 5] = ['p', 'w', 'c', 'p', 'd'];
